71430b200361ad4030ad3a995280ca0f4dab9ebe
Leonardo Sborz <lecosb@gmail.com>
2022-05-18 14:35:15 -0300

    Injetando dados na view

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from recipes.views import contato, home, sobre
+
+from . import views
 
 urlpatterns = [
-    path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipe),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={'recipes': [make_recipe() for _ in range(10)], })
 
 
-def sobre(request):
-    return HttpResponse('Sobre')
-
-
-def contato(request):
-    return HttpResponse('Contato')
+def recipe(request, id):
+    return render(request, 'recipes/recipe-view.html', context={'recipe': [make_recipe()], })
